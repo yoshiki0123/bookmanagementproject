02d77535afecd8509caa613487c2a9b419df3242
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-10-05 18:49:34 +0900

    catalogappの機能をbookmanagementに統合

--- bookmanagementapp/urls.py
+++ bookmanagementapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import login_view, signup_view, home_view, borrow_book, return_book, logout_view
+from .views import login_view, signup_view, home_view, borrow_book, return_book, logout_view, book_list, add_book, loan_status_list
 
 app_name = 'bookmanagement'
 
@@ -10,5 +10,8 @@ urlpatterns = [
     path("home/", home_view, name="home"),
     path('borrow/', borrow_book, name='borrow'),
     path("return/", return_book, name="return"),
+    path('', book_list, name='book_list'),  # 書籍一覧
+    path('add_book/', add_book, name='add_book'),  # 書籍登録
+    path('loan_status/', loan_status_list, name='loan_status'),  # 貸出状況
     path("logout/", logout_view, name='logout'),
 ]
--- bookmanagementapp/views.py
+++ bookmanagementapp/views.py
@@ -218,5 +218,59 @@ def return_book(request):
         'q': query,
     })
 
+def book_list(request):
+    query = request.GET.get("q", "") # 検索クエリ
+    page  = request.GET.get("page", 1) # ページ番号
 
+    # DBに登録されている書籍を取得
+    books = BookModel.objects.all().order_by('isbn')
 
+    # 検索クエリがあればタイトルにフィルタを適用
+    if query:
+        books = books.filter(title__icontains=query.strip())
+
+    # ページネーション処理
+    paginator = Paginator(books, 15)  # 1ページ15件
+    try:
+        books_page = paginator.get_page(page)  
+    except (PageNotAnInteger, EmptyPage):
+        books_page = paginator.get_page(1)
+
+    return render(request, 'bookmanagementapp/book_list.html', {
+        'books_page': books_page,  
+        'q': query,
+    })
+
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        isbn = request.POST.get('isbn')
+
+        # BookModelテーブルに書籍があるか確認
+        if not BookModel.objects.filter(isbn=isbn).exists():
+            # 書籍がない場合は登録処理を実施
+            try:
+                # BookModelテーブルにレコードを作成
+                BookModel.objects.create(title=title, author=author, isbn=isbn)
+                messages.success(request, f"「{title}」の登録を完了しました。")
+                return redirect('bookmanagement:add_book')
+            except IntegrityError:
+                # レコード作成に失敗した場合はメッセージを表示
+                messages.warning(request, f"「{title}」登録に失敗しました。")
+        else:
+            #　書籍がある場合はメッセージを表示
+            messages.info(request, f"「{title}」は既に登録済みです。")
+            return redirect('bookmanagement:add_book')
+            
+        # エラー時のみ値を保持して再表示
+        return render(request, 'bookmanagementapp/add_book.html', {
+            'title': title, 'author': author, 'isbn': isbn
+        })
+
+    return render(request, 'bookmanagementapp/add_book.html')
+
+def loan_status_list(request):
+    # 貸出状況の一覧を取得
+    loans = LoanModel.objects.filter(returned_date__isnull=True)
+    return render(request, 'bookmanagementapp/loan_status.html', {'loans': loans})
